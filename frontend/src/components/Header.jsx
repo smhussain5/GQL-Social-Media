51c65d116116e5166da01228bab32df3cc02d841
smhussain5 <139831097+smhussain5@users.noreply.github.com>
2024-06-01 23:12:40 -0500

    WIP NAVBAR

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,10 +1,23 @@
 // import * as React from 'react';
+import { AppBar, Button, IconButton, Stack, Toolbar, Typography } from '@mui/material';
+import ElectricBoltIcon from '@mui/icons-material/ElectricBolt';
 
 export function Header() {
     return (
-        <div>
-            HEADER
-        </div>
+        <AppBar position='static' color='default'>
+            <Toolbar>
+                <IconButton href='/' size='large' edge='start' color='inherit'>
+                    <ElectricBoltIcon />
+                </IconButton>
+                <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
+                    GQL
+                </Typography>
+                <Stack direction="row" spacing={2}>
+                    <Button href='/' color='primary' variant='contained'>Home</Button>
+                    <Button href='/profile' color='primary' variant='contained'>Profile</Button>
+                </Stack>
+            </Toolbar>
+        </AppBar >
     );
 }
 
